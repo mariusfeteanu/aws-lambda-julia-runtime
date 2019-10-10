5528b8201232d4aa9f73be19e0da85753b3f93c6
Marius Feteanu <marius.feteanu@gmail.com>
2019-10-10 16:31:15 +0100

    make a smaller build

--- julia-build/build.sh
+++ julia-build/build.sh
@@ -1,6 +1,12 @@
-echo "override JULIA_PRECOMPILE = 0" > Make.user
-echo "override JULIA_CPU_TARGET = generic;native" >> Make.user
-make
+# echo "override JULIA_PRECOMPILE = 0" > Make.user
+# https://docs.julialang.org/en/v1/devdocs/sysimg/index.html
+
+echo "override JULIA_CPU_TARGET = x86-64" > Make.user
+
+cat Make.user
+make clean
+make -j 4 binary-dist
+ls -lh julia-1.2.0-linux64.tar.gz
 
 # 77 MB with precompile
 # 68 MB without precompile
---
+++ julia-build/sysimg.jl
@@ -0,0 +1,98 @@
+# This file is a part of Julia. License is MIT: https://julialang.org/license
+
+Core.include(Main, "Base.jl")
+
+using .Base
+
+# Ensure this file is also tracked
+pushfirst!(Base._included_files, (@__MODULE__, joinpath(@__DIR__, "Base.jl")))
+pushfirst!(Base._included_files, (@__MODULE__, joinpath(@__DIR__, "sysimg.jl")))
+
+# set up depot & load paths to be able to find stdlib packages
+@eval Base creating_sysimg = true
+Base.init_depot_path()
+Base.init_load_path()
+
+if false  # Base.is_primary_base_module
+# load some stdlib packages but don't put their names in Main
+let
+    # Stdlibs manually sorted in top down order
+    stdlibs = [
+            # No deps
+            :Base64,
+            :CRC32c,
+            :SHA,
+            :FileWatching,
+            :Unicode,
+            :Mmap,
+            :Serialization,
+            :Libdl,
+            :Markdown,
+            :LibGit2,
+            :Logging,
+            :Sockets,
+            :Printf,
+            :Profile,
+            :Dates,
+            :DelimitedFiles,
+            :Random,
+            :UUIDs,
+            :Future,
+            :LinearAlgebra,
+            :SparseArrays,
+            :SuiteSparse,
+            :Distributed,
+            :SharedArrays,
+            :Pkg,
+            :Test,
+            :REPL,
+            :Statistics,
+        ]
+
+    maxlen = maximum(textwidth.(string.(stdlibs)))
+
+    print_time = (mod, t) -> (print(rpad(string(mod) * "  ", maxlen + 3, "─")); Base.time_print(t * 10^9); println())
+    print_time(Base, (Base.end_base_include - Base.start_base_include) * 10^(-9))
+
+    Base._track_dependencies[] = true
+    Base.tot_time_stdlib[] = @elapsed for stdlib in stdlibs
+        tt = @elapsed Base.require(Base, stdlib)
+        print_time(stdlib, tt)
+    end
+    for dep in Base._require_dependencies
+        continue
+        dep[3] == 0.0 && continue
+        push!(Base._included_files, dep[1:2])
+    end
+    empty!(Base._require_dependencies)
+    Base._track_dependencies[] = false
+
+    print_time("Stdlibs total", Base.tot_time_stdlib[])
+end
+end
+
+# Clear global state
+empty!(Core.ARGS)
+empty!(Base.ARGS)
+empty!(LOAD_PATH)
+@eval Base creating_sysimg = false
+Base.init_load_path() # want to be able to find external packages in userimg.jl
+
+let
+tot_time_userimg = @elapsed (Base.isfile("userimg.jl") && Base.include(Main, "userimg.jl"))
+
+
+tot_time_base = (Base.end_base_include - Base.start_base_include) * 10.0^(-9)
+tot_time = tot_time_base + Base.tot_time_stdlib[] + tot_time_userimg
+
+println("Sysimage built. Summary:")
+print("Total ─────── "); Base.time_print(tot_time               * 10^9); print(" \n");
+print("Base: ─────── "); Base.time_print(tot_time_base          * 10^9); print(" "); show(IOContext(stdout, :compact=>true), (tot_time_base          / tot_time) * 100); println("%")
+print("Stdlibs: ──── "); Base.time_print(Base.tot_time_stdlib[] * 10^9); print(" "); show(IOContext(stdout, :compact=>true), (Base.tot_time_stdlib[] / tot_time) * 100); println("%")
+if isfile("userimg.jl")
+print("Userimg: ──── "); Base.time_print(tot_time_userimg       * 10^9); print(" "); show(IOContext(stdout, :compact=>true), (tot_time_userimg       / tot_time) * 100); println("%")
+end
+end
+
+empty!(LOAD_PATH)
+empty!(DEPOT_PATH)
